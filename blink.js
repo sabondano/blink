66370d3063ba104465231b9ca506045030cf6bc9
Sebastian Abondano <sabondano1@gmail.com>
2015-10-17 19:32:56 -0600

    first draft

--- blink.js
+++ blink.js
@@ -1,1 +1,16 @@
 // YOUR CODE GOES HERE
+(function( $ ) {
+
+  $.fn.blink = function(milliseconds) {
+
+    this.each(function() {
+
+      var target = this.className;
+
+      setInterval(function() { $("." + target).toggle(); }, milliseconds);
+
+    });
+
+  };
+
+}(jQuery));
